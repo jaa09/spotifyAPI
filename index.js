d082f63b471574858edbe84f9e15bd843fc1247c
jaa09 <sabriflorin@gmail.com>
2021-11-26 02:21:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
  * For more information, read
  * https://developer.spotify.com/web-api/authorization-guide/#client_credentials_flow
  */
-
+/*
 var request = require('request'); // "Request" library
 
 var client_id = '36e2a0dafc2643ddb805986cc7953828'; // Your client id
@@ -40,4 +40,24 @@ request.post(authOptions, function(error, response, body) {
       console.log(body);
     });
   }
+});
+*/
+var client_id = '36e2a0dafc2643ddb805986cc7953828';
+var redirect_uri = 'http://localhost:3000/callback';
+
+var app = express();
+
+app.get('/login', function(req, res) {
+
+  var state = generateRandomString(16);
+  var scope = 'user-read-private user-read-email';
+
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
 });
